0ebe8b5982dd669517e90588903d5b9d2cfb8f35
Rodrigo Pedroza <rodrigo.pedroza@Ptg-ws40.protagon.com.br>
2015-06-02 12:42:06 -0300

    vericação.

--- system/funcoes/js/f_ranking.js
+++ system/funcoes/js/f_ranking.js
@@ -5,3 +5,19 @@
  */
 
 
+//vjQuery(document).ready(function() {
+//    //Inicio Mascara Telefone
+//    jQuery('input[type=tel]').mask("(99) 9999-9999?9")  
+//        .live('focusout', function (event) {  
+//            var target, phone, element;  
+//            target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
+//            phone = target.value.replace(/\D/g, '');  
+//            element = $(target);  
+//            element.unmask();  
+//            if(phone.length > 10) {  
+//                element.mask("(99) 99999-999?9");  
+//            } else {  
+//                element.mask("(99) 9999-9999?9");  
+//            } 
+//	});
+//});
