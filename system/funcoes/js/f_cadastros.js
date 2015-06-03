fdb8b599f503be29c2bea8679bf7b86beb4a609a
Rodrigo Pedroza <rodrigo.pedroza@Ptg-ws40.protagon.com.br>
2015-06-03 12:29:35 -0300

    Modificação da mascara do telefone.

--- system/funcoes/js/f_cadastros.js
+++ system/funcoes/js/f_cadastros.js
@@ -137,7 +137,31 @@ $(document).ready(function ()
     });
 });
 
-$(function ($) {
-    $("#tel").mask("(99) 9999-9999");
-    $("#telefone_professor").mask("(99) 9999-9999");
-});
+//$(function ($) {
+//   // $("#tel").mask("(99) 9999?-9999");
+//
+//    $("#telefone_professor").mask("(99) 9999?-9999");
+//});
+$('#tel').focusout(function(){
+    var phone, element;
+    element = $(this);
+    element.unmask();
+    phone = element.val().replace(/\D/g, '');
+    if(phone.length > 10) {
+        element.mask("(99) 99999-999?9");
+    } else {
+        element.mask("(99) 9999-9999?9");
+    }
+}).trigger('focusout');
+
+$('#telefone_professor').focusout(function(){
+    var phone, element;
+    element = $(this);
+    element.unmask();
+    phone = element.val().replace(/\D/g, '');
+    if(phone.length > 10) {
+        element.mask("(99) 99999-999?9");
+    } else {
+        element.mask("(99) 9999-9999?9");
+    }
+}).trigger('focusout');
